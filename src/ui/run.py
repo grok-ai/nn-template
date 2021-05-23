83d764bb6bac0b58e31bfd5f7b35cd1847b32bf0
Valentino Maiorca <valentino@maiorca.xyz>
2021-05-23 10:27:21 +0200

    Check for the mandatory wandb login on the UI.

--- src/ui/run.py
+++ src/ui/run.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import streamlit as st
+import wandb
 
 from src.pl_modules.model import MyModel
 from src.ui.ui_utils import select_checkpoint
@@ -10,6 +11,12 @@ from src.ui.ui_utils import select_checkpoint
 def get_model(checkpoint_path: Path):
     return MyModel.load_from_checkpoint(checkpoint_path=str(checkpoint_path))
 
+
+if wandb.api.api_key is None:
+    st.error("You are not logged in on `Weights and Biases`: https://docs.wandb.ai/ref/cli/wandb-login")
+    st.stop()
+
+st.sidebar.subheader(f"Logged in W&B as: {wandb.api.viewer()['entity']}")
 
 checkpoint_path = select_checkpoint()
 model: MyModel = get_model(checkpoint_path=checkpoint_path)
