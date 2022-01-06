34218c2e1a2fa0d3f4cbd7cbab9a31d55fb9099b
Luca Moschella <luca.moschella94@gmail.com>
2022-01-06 10:27:13 +0100

    Import the version as __version__ attribute in the package

---
+++ src/nn_template/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from ._version import __version__ as __version__
+except ImportError:
+    import sys
+
+    print(
+        "Project not installed in the current env, activate the correct env or install it with:\n\tpip install -e .",
+        file=sys.stderr,
+    )
+    __version__ = "unknown"
