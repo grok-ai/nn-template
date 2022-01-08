183fda22bfedf94690e67d86a8c02812b4d5c858
Luca Moschella <luca.moschella94@gmail.com>
2022-01-08 16:51:10 +0100

    Optimize fixture scoping

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from hydra import compose, initialize
 from hydra.core.hydra_config import HydraConfig
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pytest import FixtureRequest, TempdirFactory
 from pytorch_lightning import seed_everything
 
@@ -14,7 +14,7 @@ seed_everything(0)
 TRAIN_MAX_NSTEPS = 1
 
 
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def cfg() -> DictConfig:
     with initialize(config_path="../conf"):
         cfg = compose(config_name="default", return_hydra_config=True)
@@ -22,8 +22,10 @@ def cfg() -> DictConfig:
         yield cfg
 
 
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def cfg_simple_train(cfg: DictConfig) -> DictConfig:
+    cfg = OmegaConf.create(cfg)
+
     # Disable logger
     cfg.train.logger.mode = "disabled"
 
@@ -39,14 +41,16 @@ def cfg_simple_train(cfg: DictConfig) -> DictConfig:
     return cfg
 
 
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def cfg_fast_dev_run(cfg_simple_train: DictConfig) -> DictConfig:
+    cfg_simple_train = OmegaConf.create(cfg_simple_train)
+
     # Enable the fast_dev_run flag
     cfg_simple_train.train.trainer.fast_dev_run = True
     return cfg_simple_train
 
 
-@pytest.fixture(params=["cfg_simple_train", "cfg_fast_dev_run"])
+@pytest.fixture(scope="package", params=["cfg_simple_train", "cfg_fast_dev_run"])
 def cfg_all_training(request: FixtureRequest):
     return request.getfixturevalue(request.param)
 
@@ -55,6 +59,8 @@ def cfg_all_training(request: FixtureRequest):
 # Test training scripts
 #
 def _test_train(tmpdir_factory: TempdirFactory, cfg_parametrized: DictConfig) -> None:
+    cfg_parametrized = OmegaConf.create(cfg_parametrized)
+
     test_train_tmpdir = tmpdir_factory.mktemp("test_train_tmpdir")
 
     # Force the wandb dir to be in the temp folder
@@ -67,6 +73,6 @@ def _test_train(tmpdir_factory: TempdirFactory, cfg_parametrized: DictConfig) ->
     test_train_tmpdir.remove()
 
 
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def run_test_train(tmpdir_factory: TempdirFactory, cfg_all_training: DictConfig):
     yield from _test_train(tmpdir_factory=tmpdir_factory, cfg_parametrized=cfg_all_training)
