0667469ee71f13f6fcfc85f8107c1a610f955100
Luca Moschella <luca.moschella94@gmail.com>
2022-01-08 12:41:11 +0100

    Add the configuration to the Lightning checkpoints (#20)

--- src/nn_template/run.py
+++ src/nn_template/run.py
@@ -8,7 +8,7 @@ import nn_template  # isort:skip # noqa
 
 import logging
 from pathlib import Path
-from typing import List
+from typing import Callable, Dict, List, NoReturn
 
 import hydra
 import omegaconf
@@ -30,6 +30,26 @@ def build_callbacks(cfg: DictConfig) -> List[Callback]:
         callbacks.append(hydra.utils.instantiate(callback, _recursive_=False))
 
     return callbacks
+
+
+class OnSaveCheckpointInjectionHook:
+    def __init__(
+        self,
+        cfg: DictConfig,
+        on_save_checkpoint: Callable[[Dict], NoReturn],
+    ):
+        """Inject the configuration into the checkpoint monkey patching the on_save_checkpoint hook.
+
+        Args:
+            cfg: the configuration to inject
+            on_save_checkpoint: the on_save_checkpoint to monkey patch
+        """
+        self.cfg = cfg
+        self.on_save_checkpoint = on_save_checkpoint
+
+    def __call__(self, checkpoint: Dict) -> None:
+        self.on_save_checkpoint(checkpoint)
+        checkpoint["cfg"] = OmegaConf.to_container(self.cfg, resolve=True)
 
 
 def run(cfg: DictConfig) -> None:
@@ -64,6 +84,7 @@ def run(cfg: DictConfig) -> None:
         cfg.nn.module,
         _recursive_=False,
     )
+    model.on_save_checkpoint = OnSaveCheckpointInjectionHook(cfg=cfg, on_save_checkpoint=model.on_save_checkpoint)
 
     # Instantiate the callbacks
     callbacks: List[Callback] = build_callbacks(cfg=cfg.train.callbacks)
