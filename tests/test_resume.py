6d3e08a0a50de15e60eb8219a72e9e96198d18ad
Luca Moschella <luca.moschella94@gmail.com>
2022-01-09 01:14:54 +0100

    Test the checkpoint can be loaded

---
+++ tests/test_resume.py
@@ -0,0 +1,20 @@
+from importlib import import_module
+from pathlib import Path
+
+from omegaconf import DictConfig
+from pytorch_lightning import LightningModule
+
+
+def test_load_checkpoint(run_trainings_not_dry: str, cfg_all_not_dry: DictConfig) -> None:
+    ckpts_path = Path(run_trainings_not_dry) / "checkpoints"
+    checkpoint_path = next(ckpts_path.glob("*"))
+    assert checkpoint_path
+
+    reference: str = cfg_all_not_dry.nn.module._target_
+    module_ref, class_ref = reference.rsplit(".", maxsplit=1)
+    module_class: LightningModule = getattr(import_module(module_ref), class_ref)
+    assert module_class is not None
+
+    module = module_class.load_from_checkpoint(checkpoint_path=checkpoint_path)
+    assert module is not None
+    assert sum(p.numel() for p in module.parameters())
