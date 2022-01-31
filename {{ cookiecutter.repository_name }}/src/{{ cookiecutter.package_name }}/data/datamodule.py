f3a3c7f35d0b00368ad9ff5e9ce563f4e2d997d1
Valentino Maiorca <valentino@maiorca.xyz>
2022-01-31 20:21:22 +0100

    Fix minor typos

--- {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/data/datamodule.py
+++ {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/data/datamodule.py
@@ -105,7 +105,7 @@ class MyDataModule(pl.LightningDataModule):
 
 @hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default")
 def main(cfg: omegaconf.DictConfig):
-    _: pl.LightningDataModule = hydra.utils.instantiate(cfg.data.datamodule, _recursive_=False)
+    _: pl.LightningDataModule = hydra.utils.instantiate(cfg.nn.data, _recursive_=False)
 
 
 if __name__ == "__main__":
