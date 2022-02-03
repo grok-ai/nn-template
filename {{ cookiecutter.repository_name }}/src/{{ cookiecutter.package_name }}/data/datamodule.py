cd5167bd7fd78d13a97ffb087a3b90f6c7683865
Luca Moschella <luca.moschella94@gmail.com>
2022-02-03 11:38:04 +0100

    Add missing docstrings (#39)
    * Add missing docstrings
    
    * Fix pre-commit errors
    
    * Update MetaData docstring
    
    Co-authored-by: Valentino Maiorca <valentino@maiorca.xyz>

--- {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/data/datamodule.py
+++ {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/data/datamodule.py
@@ -16,9 +16,35 @@ pylogger = logging.getLogger(__name__)
 
 class MetaData:
     def __init__(self, class_vocab: Mapping[str, int]):
+        """The data information the Lightning Module will be provided with.
+
+        This is a "bridge" between the Lightning DataModule and the Lightning Module.
+        There is no constraint on the class name nor in the stored information, as long as it exposes the
+        `save` and `load` methods.
+
+        The Lightning Module will receive an instance of MetaData when instantiated,
+        both in the train loop or when restored from a checkpoint.
+
+        This decoupling allows the architecture to be parametric (e.g. in the number of classes) and
+        DataModule/Trainer independent (useful in prediction scenarios).
+        MetaData should contain all the information needed at test time, derived from its train dataset.
+
+        Examples are the class names in a classification task or the vocabulary in NLP tasks.
+        MetaData exposes `save` and `load`. Those are two user-defined methods that specify
+        how to serialize and de-serialize the information contained in its attributes.
+        This is needed for the checkpointing restore to work properly.
+
+        Args:
+            class_vocab: association between class names and their indices
+        """
         self.class_vocab: Mapping[str, int] = class_vocab
 
-    def save(self, dst_path: Path):
+    def save(self, dst_path: Path) -> None:
+        """Serialize the MetaData attributes into the zipped checkpoint in dst_path.
+
+        Args:
+            dst_path: the root folder of the metadata inside the zipped checkpoint
+        """
         pylogger.debug(f"Saving MetaData to '{dst_path}'")
 
         (dst_path / "class_vocab.tsv").write_text(
@@ -27,6 +53,14 @@ class MetaData:
 
     @staticmethod
     def load(src_path: Path) -> "MetaData":
+        """Deserialize the MetaData from the information contained inside the zipped checkpoint in src_path.
+
+        Args:
+            src_path: the root folder of the metadata inside the zipped checkpoint
+
+        Returns:
+            an instance of MetaData containing the information in the checkpoint
+        """
         pylogger.debug(f"Loading MetaData from '{src_path}'")
 
         lines = (src_path / "class_vocab.tsv").read_text(encoding="utf-8").splitlines()
@@ -150,7 +184,12 @@ class MyDataModule(pl.LightningDataModule):
 
 
 @hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default")
-def main(cfg: omegaconf.DictConfig):
+def main(cfg: omegaconf.DictConfig) -> None:
+    """Debug main to quickly develop the DataModule.
+
+    Args:
+        cfg: the hydra configuration
+    """
     _: pl.LightningDataModule = hydra.utils.instantiate(cfg.data.datamodule, _recursive_=False)
 
 
