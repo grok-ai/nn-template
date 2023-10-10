16e46b68fa3652fee4f67e818ef57eb199c72f62
Luca Moschella <luca.moschella94@gmail.com>
2023-10-10 12:31:37 +0200

    Add hf datasets integration

--- {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/pl_modules/pl_module.py
+++ {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/pl_modules/pl_module.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, Mapping, Optional, Sequence, Tuple, Union
 
 import hydra
 import omegaconf
@@ -13,7 +13,6 @@ from nn_core.common import PROJECT_ROOT
 from nn_core.model_logging import NNLogger
 
 from {{ cookiecutter.package_name }}.data.datamodule import MetaData
-from {{ cookiecutter.package_name }}.modules.module import CNN
 
 pylogger = logging.getLogger(__name__)
 
@@ -21,7 +20,7 @@ pylogger = logging.getLogger(__name__)
 class MyLightningModule(pl.LightningModule):
     logger: NNLogger
 
-    def __init__(self, metadata: Optional[MetaData] = None, *args, **kwargs) -> None:
+    def __init__(self, model, metadata: Optional[MetaData] = None, *args, **kwargs) -> None:
         super().__init__()
 
         # Populate self.hparams with args and kwargs automagically!
@@ -36,11 +35,11 @@ class MyLightningModule(pl.LightningModule):
             task="multiclass",
             num_classes=len(metadata.class_vocab) if metadata is not None else None,
         )
-        self.train_accuracy = metric.clone()
-        self.val_accuracy = metric.clone()
-        self.test_accuracy = metric.clone()
+        self.train_acc = metric.clone()
+        self.val_acc = metric.clone()
+        self.test_acc = metric.clone()
 
-        self.model = CNN(num_classes=len(metadata.class_vocab))
+        self.model = hydra.utils.instantiate(model, num_classes=len(metadata.class_vocab))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Method for the forward pass.
@@ -54,74 +53,36 @@ class MyLightningModule(pl.LightningModule):
         # example
         return self.model(x)
 
-    def step(self, x, y) -> Mapping[str, Any]:
+    def _step(self, batch: Dict[str, torch.Tensor], split: str) -> Mapping[str, Any]:
+        x = batch[self.hparams.x_key]
+        gt_y = batch[self.hparams.y_key]
+
         # example
         logits = self(x)
-        loss = F.cross_entropy(logits, y)
+        loss = F.cross_entropy(logits, gt_y)
+        preds = torch.softmax(logits, dim=-1)
+
+        metrics = getattr(self, f"{split}_acc")
+        metrics.update(preds, gt_y)
+
+        self.log_dict(
+            {
+                f"acc/{split}": metrics,
+                f"loss/{split}": loss,
+            },
+            on_epoch=True,
+        )
+
         return {"logits": logits.detach(), "loss": loss}
 
     def training_step(self, batch: Any, batch_idx: int) -> Mapping[str, Any]:
-        # example
-        x, y = batch
-        step_out = self.step(x, y)
-
-        self.log_dict(
-            {"loss/train": step_out["loss"].cpu().detach()},
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-        )
-
-        self.train_accuracy(torch.softmax(step_out["logits"], dim=-1), y)
-        self.log_dict(
-            {
-                "acc/train": self.train_accuracy,
-            },
-            on_epoch=True,
-        )
-
-        return step_out
+        return self._step(batch=batch, split="train")
 
     def validation_step(self, batch: Any, batch_idx: int) -> Mapping[str, Any]:
-        # example
-        x, y = batch
-        step_out = self.step(x, y)
-
-        self.log_dict(
-            {"loss/val": step_out["loss"].cpu().detach()},
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-        )
-
-        self.val_accuracy(torch.softmax(step_out["logits"], dim=-1), y)
-        self.log_dict(
-            {
-                "acc/val": self.val_accuracy,
-            },
-            on_epoch=True,
-        )
-
-        return step_out
+        return self._step(batch=batch, split="val")
 
     def test_step(self, batch: Any, batch_idx: int) -> Mapping[str, Any]:
-        # example
-        x, y = batch
-        step_out = self.step(x, y)
-
-        self.log_dict(
-            {"loss/test": step_out["loss"].cpu().detach()},
-        )
-
-        self.test_accuracy(torch.softmax(step_out["logits"], dim=-1), y)
-        self.log_dict(
-            {
-                "acc/test": self.test_accuracy,
-            },
-            on_epoch=True,
-        )
-
-        return step_out
+        return self._step(batch=batch, split="test")
 
     def configure_optimizers(
         self,
@@ -154,13 +115,8 @@ def main(cfg: omegaconf.DictConfig) -> None:
     Args:
         cfg: the hydra configuration
     """
-    module = cfg.nn.module
-    _: pl.LightningModule = hydra.utils.instantiate(
-        module,
-        optim=module.optimizer,
-        metadata=MetaData(class_vocab={str(i): i for i in range(10)}),
-        _recursive_=False,
-    )
+    m: pl.LightningDataModule = hydra.utils.instantiate(cfg.nn.data, _recursive_=False)
+    _: pl.LightningModule = hydra.utils.instantiate(cfg.nn.module, _recursive_=False, metadata=m.metadata)
 
 
 if __name__ == "__main__":
---
+++ {{ cookiecutter.repository_name }}/src/{{ cookiecutter.package_name }}/utils/hf_io.py
@@ -0,0 +1,113 @@
+from collections import namedtuple
+from pathlib import Path
+from typing import Any, Dict
+
+from anypy.data.metadata_dataset_dict import MetadataDatasetDict
+from datasets import Dataset, DatasetDict, load_dataset, load_from_disk
+
+from nn_core.common import PROJECT_ROOT
+
+DatasetParams = namedtuple("DatasetParams", ["name", "fine_grained", "train_split", "test_split", "hf_key"])
+
+
+class HFTransforms:
+    def __init__(
+        self,
+        key: str,
+        transforms: Dict,
+    ):
+        self.transforms = transforms
+        self.key = key
+
+    def __call__(self, samples: Dict[str, Any]) -> Dict[str, Any]:
+        samples[self.key] = [self.transforms(data) for data in samples[self.key]]
+        return samples
+
+    def __repr__(self) -> str:
+        return repr(self.transforms)
+
+
+def preprocess_dataset(
+    dataset: Dataset,
+    cfg: Dict,
+) -> Dataset:
+    dataset = dataset.rename_column(cfg["label_key"], cfg["standard_y_key"])
+    dataset = dataset.rename_column(cfg["image_key"], cfg["standard_x_key"])
+
+    return dataset
+
+
+# def add_ids_to_dataset(dataset):
+#     N = len(dataset["train"])
+#     M = len(dataset["test"])
+#     indices = {"train": list(range(N)), "test": list(range(N, N + M))}
+
+#     for mode in ["train", "test"]:
+#         dataset[mode] = dataset[mode].map(
+#             lambda _, ind: {"id": indices[mode][ind]},
+#             with_indices=True,
+#             keep_in_memory=True,
+#             load_from_cache_file=False,
+#         )
+
+#     return dataset
+
+
+def save_dataset_to_disk(dataset: MetadataDatasetDict, output_path: Path):
+    if not isinstance(output_path, Path):
+        output_path = Path(output_path)
+
+    if not output_path.exists():
+        output_path.mkdir(parents=True)
+
+    dataset.save_to_disk(output_path)
+
+
+def load_hf_dataset(**cfg):
+    dataset_params: DatasetParams = DatasetParams(
+        cfg["ref"],
+        None,
+        cfg["train_split"],
+        cfg["test_split"],
+        (cfg["ref"],),
+    )
+    DATASET_KEY = "_".join(
+        map(
+            str,
+            [v for k, v in dataset_params._asdict().items() if k != "hf_key" and v is not None],
+        )
+    )
+    DATASET_DIR: Path = PROJECT_ROOT / "data" / "datasets" / DATASET_KEY
+
+    if not DATASET_DIR.exists():
+        train_dataset = load_dataset(
+            dataset_params.name,
+            split=dataset_params.train_split,
+            use_auth_token=True,
+        )
+        if "val_percentage" in cfg:
+            train_val_dataset = train_dataset.train_test_split(test_size=cfg["val_percentage"], shuffle=True)
+            train_dataset = train_val_dataset["train"]
+            val_dataset = train_val_dataset["test"]
+        elif "val_split" in cfg:
+            val_dataset = load_dataset(dataset_params.name, split=cfg["val_split"])
+        else:
+            raise RuntimeError("Either val_percentage or val_split must be specified in the config.")
+
+        test_dataset = load_dataset(dataset_params.name, split=dataset_params.test_split)
+
+        dataset: DatasetDict = MetadataDatasetDict(
+            train=train_dataset,
+            val=val_dataset,
+            test=test_dataset,
+        )
+
+        dataset = preprocess_dataset(dataset, cfg)
+        # TODO: If adding ids, ensure consistent ids across splits (even random val ones)
+        # dataset = add_ids_to_dataset(dataset)
+
+        save_dataset_to_disk(dataset, DATASET_DIR)
+    else:
+        dataset: Dataset = load_from_disk(dataset_path=str(DATASET_DIR))
+
+    return dataset
